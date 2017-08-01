3898d29f6b989d9e9302b369c6308ba33349a456
SigalKr <sigal.krendel@gmail.com>
2017-08-01 22:44:01 +0300

    Initial

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import main from './sass/main.scss';
+import $ from 'jquery';
+import 'slick-carousel';
+
+$('.gallery__photos').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+
